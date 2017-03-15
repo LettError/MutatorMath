a643797cb5ead46afe9074297070babb0424244d
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-03-15 15:48:10 +0000

    Bump version: 2.0.3 → 2.0.4.dev0

--- Lib/mutatorMath/__init__.py
+++ Lib/mutatorMath/__init__.py
@@ -33,7 +33,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
 
-__version__ = "2.0.3"
+__version__ = "2.0.4.dev0"
 
 from mutatorMath.objects.location import Location
 from mutatorMath.objects.mutator import Mutator
