6708163faaafd88bb6220b422c134dda437055c7
Cosimo Lupo <cosimo@anthrotype.com>
2018-06-07 11:00:01 +0100

    Release 2.1.1
    - Assume incoming locations might be plain dicts and wrap them (#110).
    - Don't fail when reading designspace documents with `format` attribute
      formatted as a float, as in latest designspace spec (#117).

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="MutatorMath",
-    version="2.1.1.dev0",
+    version="2.1.1",
     description=("Python for piecewise linear interpolation in multiple "
                  "dimensions with multiple, arbitrarily placed, masters."),
     long_description=long_description,
