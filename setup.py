a0d018e1701dc52ad67acfc95201794fa1ef17a2
Cosimo Lupo <cosimo@anthrotype.com>
2018-11-01 19:34:33 +0000

    Release 2.1.2
    - Removed ufoLib dependency. Bumped minimum fonttools >= 3.32.0.
    - Added ``bend=True`` flag for makeInstance; allows instances to be
      calculated without the bender transformation.
      This is sometimes necessary.

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="MutatorMath",
-    version="2.1.2.dev0",
+    version="2.1.2",
     description=("Python for piecewise linear interpolation in multiple "
                  "dimensions with multiple, arbitrarily placed, masters."),
     long_description=long_description,
